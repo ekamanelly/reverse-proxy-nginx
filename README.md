8995cefb2521b0abc7cc9091f2d68ff16d33aaf1
ekamanelly <ekamanelly@gmail.com>
2022-11-04 22:39:15 +0100

    starter reverse proxy

---
+++ README.md
@@ -0,0 +1,38 @@
+# devops-django-react-task
+
+## The entire app workflow 
+
+clone repo
+cd into the primary folder
+run the `install-docker-n-compose-container.sh` too install docker on host and spin up docker compose
+or 
+intall docker manually, digital ocean had a great docs 
+then run `docker-compose up`
+
+
+## Backend development workflow
+
+```json
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+python manage.py runserver
+```
+
+## Frontend development workflow
+
+You are to update your name in ./frontend/components/App.js
+
+```json
+npm i
+npm start
+```
+
+## For deploying
+
+```json
+npm run build
+```
+
+It should look like this if successful
+<img width="1440" alt="Screen Shot 2022-11-02 at 19 30 22" src="https://user-images.githubusercontent.com/66765302/199572589-43bd05b7-95a6-455c-bc25-3cd437c95339.png">
---
+++ install-docker-n-compose-container.sh
@@ -0,0 +1,31 @@
+ 
+ 
+ # update and install apt package curl gnupg  
+ sudo apt-get update -y
+ sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+ # Add Docker’s official GPG key:
+sudo mkdir -p /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg   
+
+# Use the following command to set up the repository
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+# install docker engine
+ sudo apt-get update -y
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+# verify docker was install
+ docker -v
+
+# only build containers with changed/ altered file 
+ docker-compose up --build -d
+
+
+ echo 
